920373661457a5d862be44eed69bf242383bb37b
MG.S <akdldkvj47@gmail.com>
2022-05-30 10:14:24 +0900

    convert unix timestamp to datetime(str)

--- service.py
+++ service.py
@@ -3,6 +3,7 @@ import pandas as pd
 from ytlv import *
 from concurrent.futures import ThreadPoolExecutor
 from operator import itemgetter
+import datetime
 
 def json_to_dict_board(result):
     global lv, channel_num, channel_name, channel_url, channel_id, published_at, birthday, inactive, debut, embed_url, state
@@ -41,6 +42,10 @@ def json_to_dict_board(result):
 def fetch(row):
     live = lv.ytid(row['channel_id'])
     live = live[0]
+    if not live['timestamp'] == 'NONE':
+        dt = datetime.datetime.fromtimestamp(int(live['timestamp']))
+        live['timestamp'] = dt.strftime("%Y-%m-%d %H:%M:%S")
+    # 나중에 현재 시간 기준으로 1주일 이내 방송만 표시 되게 끔 수정할 예정임
     channel_num.append(row['channel_num'])
     channel_name.append(row['channel_name'])
     channel_url.append(row['channel_url'])
